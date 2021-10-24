d8812ddf87fce6ccdba163a4f0cbf5536d14ed69
Nouu <noelarrieulacoste@yahoo.fr>
2021-10-24 14:29:49 +0200

    optimize event display

--- src/commands/calendar-sync.ts
+++ src/commands/calendar-sync.ts
@@ -58,15 +58,25 @@ async function action(days: string, options: CommandOptions) {
       return;
     }
 
+    agenda = agenda.sort((a, b) => a.start_date - b.start_date);
+
     for (const agendaItem of agenda) {
       const start = new Date(agendaItem.start_date);
       const end = new Date(agendaItem.end_date);
+      const course = agendaItem.name;
+      const teacher = agendaItem.teacher;
+      const date = format(start, 'EEEE d MMMM yyyy', {
+        locale: fr,
+      });
+      const startTime = format(start, 'kk:mm');
+      const endTime = format(end, 'kk:mm');
       console.log(
-        `Cours : ${agendaItem.name}, Date : ${format(
-          start,
-          'EEEE d MMMM yyyy',
-          { locale: fr },
-        )} (${format(start, 'kk:mm')} - ${format(end, 'kk:mm')})`,
+        `Cours : ${course} | Intervenant : ${teacher} | Date : ${date} (${startTime} - ${endTime}) | Lieux : ${agendaItem.rooms?.map(
+          (room) =>
+            ` ${room.campus.toUpperCase()} ${room.name} ${room.latitude},${
+              room.longitude
+            }`,
+        )}`,
       );
     }
   } catch (e) {
