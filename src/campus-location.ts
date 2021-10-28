86daa98739aac792401914b966d454d81a2c4fce
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-10-28 13:50:33 +0200

    add campus location

---
+++ src/campus-location.ts
@@ -0,0 +1,13 @@
+export const locations = new Map([
+  ['NATION1', '242 rue du Faubourg Saint Antoine, 75012 Paris'],
+  ['NATION2', '220 rue du Faubourg Saint Antoine, 75012 Paris'],
+  ['VOLTAIRE1', '1 rue Bouvier, 75011 Paris'],
+  ['VOLTAIRE2', '20 rue Bouvier, 75011 Paris'],
+  ['ERARD', '19-21 rue Erard, 75011 Paris'],
+  ['BEAUGRENELLE', '35 quai André Citroen 75015 Paris'],
+  ['MONTSOURIS', '5 rue Lemaignan, 75014 Paris'],
+  ['MONTROUGE', '11 rue Camille Pelletan, 92120 Montrouge'],
+  ['JOURDAN', '6-10 bd Jourdan 75014 Paris'],
+  ['VAUGIRARD', '273-277 rue de Vaugirard, 75012 Paris'],
+  ['MAIN D\'OR', '8‐14 Passage de la Main d’Or 75011 Paris'],
+]);
