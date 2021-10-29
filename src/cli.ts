8672187a1f1f6827091c24308040633d6bcd602b
Nouu <noelarrieulacoste@yahoo.fr>
2021-10-29 11:24:09 +0200

    restore cli

---
+++ src/cli.ts
@@ -0,0 +1,24 @@
+import updateNotifier from 'update-notifier';
+import * as commands from './commands';
+import * as commandsBase from './commands-base';
+import { config } from 'dotenv';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pkg = require('../package.json');
+
+config();
+const notifier = updateNotifier({
+  pkg,
+  shouldNotifyInNpmScript: true,
+});
+
+notifier.notify({ isGlobal: true });
+
+const program = commandsBase.init();
+commands.register(program);
+
+if (process.argv.length < 3) {
+  program.help();
+} else {
+  program.parse(process.argv);
+}
